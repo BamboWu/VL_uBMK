1ef99663eaad085adaa67bf78b426056bdf4422c
Jonathan C. Beard <jonathan.c.beard@gmail.com>
2019-08-16 10:03:24 -0500

    updates to boost benchmark, now gets the right numbers on haswell at least :), will take a look at the vl one and see if we can do the same thing

--- Makefile
+++ Makefile
@@ -2,33 +2,42 @@ AR = ar
 CC = gcc
 CXX = g++
 CCFLAGS = -O3 -g -Wall -std=c89
-CXXFLAGS = -O3 -g -Wall -std=c++11
+CXXFLAGS = -O3 -g -Wall -std=gnu++14
 SOFLAGS = -fPIC -shared
 
 THREAD_LIB = -pthread -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
 # https://stackoverflow.com/questions/35116327/when-g-static-link-pthread-cause-segmentation-fault-why
 
-BOOST_PATH = /benchmarks/VirtualLink/boost_1_63_0
-BOOST_INC = -I$(BOOST_PATH)
-BOOST_LIB = -L$(BOOST_PATH)/stage_aarch64/lib -latomic
-
+ifeq (${type}, boost)
+#BOOST_PATH = /benchmarks/VirtualLink/boost_1_63_0
+BOOST_INC = #-I$(BOOST_PATH)
+BOOST_LIB = #-L$(BOOST_PATH)/stage_aarch64/lib -latomic
+else
 VL_PATH = /benchmarks/VirtualLink/libvl
 VL_INC = -I$(VL_PATH)
 VL_LIB = -L$(VL_PATH) -lvl
+endif 
 
+ifeq ( ${sim}, true )
 GEM5_PATH = /benchmarks/VirtualLink/gem5
 ifdef GEM5
 	GEM5_DEF = -DGEM5=1
 	GEM5_INC = -I$(GEM5_PATH)/include
 	GEM5_LIB = -L$(GEM5_PATH)/util/m5 -lm5
 endif
+endif 
 
-CCFLAGS += $(GEM5_DEF) $(BOOST_INC) $(VL_INC) $(GEM5_INC)
-CXXFLAGS += $(GEM5_DEF) $(BOOST_INC) $(VL_INC) $(GEM5_INC)
+CXXOBJS = pingpong affinity
+CXXFILES= $(addsuffix .cpp, $(CXXOBJS))
+OBJS	= $(addsuffix .o, $(CXXOBJS))
 
-pingponga: pingpong.cpp
-	$(CXX) $(CXXFLAGS) -static $< -o $@ $(BOOST_LIB) $(VL_LIB) $(GEM5_LIB) $(THREAD_LIB)
+CFLAGS 		+= $(GEM5_DEF) $(BOOST_INC) $(VL_INC) $(GEM5_INC)
+CXXFLAGS 	+= $(GEM5_DEF) $(BOOST_INC) $(VL_INC) $(GEM5_INC)
+
+pingpong: $(CXXFILES)
+	$(MAKE) $(OBJS)
+	$(CXX) $(CXXFLAGS) -static $(OBJS) -o pingpong $(BOOST_LIB) $(VL_LIB) $(GEM5_LIB) $(THREAD_LIB)
 
 .PHONY: clean
 clean:
-	rm -rf pingponga *.o
+	rm -rf pingpong *.o
--- pingpong.cpp
+++ pingpong.cpp
@@ -5,142 +5,151 @@
 #include <chrono>
 #include <array>
 #include <time.h>
-#include <boost/lockfree/queue.hpp>
+#include <cstdint>
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <cstdlib>
+
+#include "affinity.hpp"
+
+#ifdef VL
 #include <vl.h>
+#else
+#include <boost/lockfree/queue.hpp>
+#endif
+
 #ifdef GEM5
 #include "gem5/m5ops.h"
 #endif
 
-#ifdef PAUSE
-#define PAUSE_ASM __asm__ volatile("pause" : : :)
-#else
-#define PAUSE_ASM __asm__ volatile("nop" : : :)
-#endif
 
 #define CAPACITY 4096
 
-typedef std::array<double, 7> ball_t;
-boost::lockfree::queue<ball_t> left_lockfree {CAPACITY / sizeof(ball_t)};
-boost::lockfree::queue<ball_t> right_lockfree {CAPACITY / sizeof(ball_t)};
+using ball_t = std::uint64_t;
 
+#ifdef VL
 int left_vl_fd;
 int right_vl_fd;
 vlendpt_t left_prod_vl, left_cons_vl, right_prod_vl, right_cons_vl;
 
-struct playerArgs {
-    bool *pwait; // all threads controlled by main for timing/statistic
-    char mech;
-    uint64_t round;
-    bool left; // left player initiate the ping-pong
+#else /** not defined VL **/
+using boost_q_t = boost::lockfree::queue< ball_t >;
+boost_q_t left_lockfree  ( CAPACITY / sizeof(ball_t) );
+boost_q_t right_lockfree ( CAPACITY / sizeof(ball_t) );
+#endif
+
+using atomic_t = std::atomic< int >;
+
+
+struct alignas( 64 ) playerArgs 
+{
+    bool                *pwait  = nullptr; // all threads controlled by main for timing/statistic
+    char                mech;
+    std::uint64_t       round;
+    bool                left; // left player initiate the ping-pong
+#ifdef VL
+
+#else
+    boost_q_t           *ql     = nullptr;
+    boost_q_t           *qr     = nullptr;
+#endif
+    char padd[ 4096 ]; /** make darned sure these aren't on the same cache line **/
 };
 
 void
-player(playerArgs *pargs) {
+player( playerArgs const *pargs, atomic_t &ready ) 
+{
 
-    const uint64_t round = pargs->round;
-    bool left = pargs->left;
-    bool my_serve = left;
-    ball_t ball;
-    uint64_t *pdouble;
-    bool valid;
-    double sum;
+    auto round          ( pargs->round  );
+    const bool left     ( pargs->left   );
+    ball_t              ball( 0 );
+    
+    affinity::set( left ? 0 : 1 );
 
-    // set affinity
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(left?0:1, &cpuset);
-    if (0 != pthread_setaffinity_np(pthread_self(),
-                                    sizeof(cpu_set_t), &cpuset)) {
-        std::cout << "\033[91mFailed to set affinity for " <<
-          pthread_self() << "\033[0m\n";
-    }
+#ifdef VL
+   vlendpt_t *psend_vl = left ? &left_prod_vl : &right_prod_vl;
+   vlendpt_t *precv_vl = left ? &right_cons_vl : &left_cons_vl;
+   while (*(pargs->pwait)) {
+     std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+   }
+   for (uint64_t r = 1; round >= r; ++r) {
+     std::cout << (left ? "L" : "R") << " @ CPU " << sched_getcpu() << "\n";
 
-    if ('f' == pargs->mech) {
-      boost::lockfree::queue<ball_t> *psend =
-        left ? &left_lockfree : &right_lockfree;
-      boost::lockfree::queue<ball_t> *precv =
-        left ? &right_lockfree : &left_lockfree;
-      while (*(pargs->pwait)) {
-        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-      }
-      for (uint64_t r = 1; round >= r; ++r) {
-        std::cout << (left ? "L" : "R") << " @ CPU " << sched_getcpu() << "\n";
-
-        if (my_serve) {
-          for (uint64_t i = 0; 7 > i; ++i) {
-            ball[i] = 0.0714 * r / round + i / 42.0;
-          }
-          psend->push(ball);
-          my_serve = false;
-        } else {
-          while(!(precv->pop(ball))) {
-            std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-          }
-          sum = 0;
-          for (uint64_t i = 0; 7 > i; ++i) {
-            sum += ball[i];
-            //std::cout << "ball[" << i << "]=" << ball[i] << std::endl;
-          }
-          std::cout << "\033[92m" << (left ? "L " : "R ") << sum <<
-            "\033[0m\n";
-          my_serve = true;
-        }
-      }
-    } else if ('V' == pargs->mech) {
-      vlendpt_t *psend_vl = left ? &left_prod_vl : &right_prod_vl;
-      vlendpt_t *precv_vl = left ? &right_cons_vl : &left_cons_vl;
-      while (*(pargs->pwait)) {
-        std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-      }
-      for (uint64_t r = 1; round >= r; ++r) {
-        std::cout << (left ? "L" : "R") << " @ CPU " << sched_getcpu() << "\n";
-
-        if (my_serve) {
-          for (uint64_t i = 0; 7 > i; ++i) {
-            ball[i] = 0.0714 * r / round + i / 42.0;
-            pdouble = (uint64_t*)&ball[i];
-            double_vl_push_strong(psend_vl, *pdouble);
-          }
-          my_serve = false;
-        } else {
-          for (uint64_t i = 0; 7 > i;) {
-            pdouble = (uint64_t*)&ball[i];
-            double_vl_pop_non(precv_vl, pdouble, &valid);
-            if (valid) {
-              i++;
-            } else {
-              std::this_thread::sleep_for(std::chrono::nanoseconds(1));
-            }
-          }
-          sum = 0;
-          for (uint64_t i = 0; 7 > i; ++i) {
-            sum += ball[i];
-            //std::cout << "ball[" << i << "]=" << ball[i] << std::endl;
-          }
-          std::cout << "\033[92m" << (left ? "L " : "R ") << sum <<
-            "\033[0m\n";
-          my_serve = true;
-        }
-      }
-    }
+     if (my_serve) {
+       for (uint64_t i = 0; 7 > i; ++i) {
+         ball[i] = 0.0714 * r / round + i / 42.0;
+         pdouble = (uint64_t*)&ball[i];
+         double_vl_push_strong(psend_vl, *pdouble);
+       }
+       my_serve = false;
+     } else {
+       for (uint64_t i = 0; 7 > i;) {
+         pdouble = (uint64_t*)&ball[i];
+         double_vl_pop_non(precv_vl, pdouble, &valid);
+         if (valid) {
+           i++;
+         } else {
+           std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+         }
+       }
+       sum = 0;
+       for (uint64_t i = 0; 7 > i; ++i) {
+         sum += ball[i];
+         //std::cout << "ball[" << i << "]=" << ball[i] << std::endl;
+       }
+       std::cout << "\033[92m" << (left ? "L " : "R ") << sum <<
+         "\033[0m\n";
+       my_serve = true;
+     }
+   }
+#else /** boost **/
+    auto *psend( left ? pargs->ql : pargs->qr );
+    auto *precv( left ? pargs->qr : pargs->ql );
+    
+    /** we're ready to start **/
+    ready++;
+    
+    while( ready != 2 ){ /** spin **/ };
+    
+    
+    /** we're ready to get started, both initialized **/
+    
+    while( --round  )
+    {
+#if DEBUG                
+    std::cout << (left ? "L" : "R") << " @ CPU " << sched_getcpu() << "\n";
+#endif
+       if( left ) 
+       {
+           while( ! psend->push( ball++ ) );
+           while( ! precv->pop( ball ) );
+       }
+       else /** not my serve **/
+       {
+           while( ! precv->pop( ball ) );
+           while( ! psend->push( ball ) );
+       }
+   } 
+#endif /** end mechanism selection **/          
+    return; /** end of player function **/
 }
 
-int main(int argc, char *argv[]) {
-
-    char mech = 'f'; // 'f' for lockfree, 'V' for Virtual Link
+int main( int argc, char **argv ) 
+{
     uint64_t round = 10;
-
-    if (1 < argc) {
-        mech = argv[1][0];
+    
+    if( argc == 2 ) 
+    {
+        round = atoll( argv[1] );
     }
-    if (2 < argc) {
-        round = atoll(argv[2]);
+    else
+    {
+        std::cerr << "usage is: " << argv[0] << " <#rounds>\n";
+        exit( EXIT_FAILURE );
     }
 
-    std::cout << argv[0] << " " << mech << " " << round << std::endl;
-
-    if ('f' == mech) { // do not mkvl() for freelock, since no rmvl() following
-    } else {
+#ifdef VL
         left_vl_fd = mkvl();
         if (0 > left_vl_fd) {
             std::cerr << "mkvl() return invalid file descriptor\n";
@@ -155,62 +164,60 @@ int main(int argc, char *argv[]) {
         }
         open_double_vl_as_producer(right_vl_fd, &right_prod_vl, 1);
         open_double_vl_as_consumer(right_vl_fd, &right_cons_vl, 1);
-    }
+#endif /** end initiation of VL **/
 
-    std::cout << "There are " << get_nprocs() << " CPUs available.\n";
+    atomic_t    ready( -1 );
 
-    bool wait = true;
     playerArgs args[2];
 
-    args[0].pwait = &wait;
-    args[0].mech = mech;
-    args[0].round = round;
-    args[0].left = true;
-    args[1].pwait = &wait;
-    args[1].mech = mech;
-    args[1].round = round;
-    args[1].left = false;
+    args[0].round   = round;
+    args[0].left    = true;
+#ifdef VL
 
-    std::thread playerl(player, &args[0]);
-    std::thread playerr(player, &args[1]);
+#else
+    args[0].ql      = &left_lockfree;
+    args[0].qr      = &right_lockfree;
+#endif
+    args[1].round   = round;
+    args[1].left    = false;
+#ifdef VL
 
-    timespec beg, end;
+#else
+    args[1].ql      = &left_lockfree;
+    args[1].qr      = &right_lockfree;
+#endif
 
-    if (clock_gettime(CLOCK_REALTIME, &beg)) {
-        std::cerr << "fail to get begin real time\n";
-    }
+
+
+    std::thread playerl( player, &args[0], std::ref( ready ) );
+    std::thread playerr( player, &args[1], std::ref( ready ) );
+    
+    const auto start( std::chrono::high_resolution_clock::now() );
 
 #ifdef GEM5
     m5_reset_stats(0, 0);
 #endif
-    wait = false;
+
+    ready++;
 
     playerl.join();
     playerr.join();
+
 #ifdef GEM5
     m5_dump_reset_stats(0, 0);
 #endif
 
-    if (clock_gettime(CLOCK_REALTIME, &end)) {
-        std::cerr << "fail to get end real time\n";
-    }
-
-    long seconds = end.tv_sec - beg.tv_sec;
-    long nanosec = end.tv_nsec - beg.tv_nsec;
-
-    if (0 > nanosec) { // underflow
-        nanosec += 1000000000;
-        --seconds;
-    }
-    std::cout << seconds << "s " << nanosec << "ns elapsed\n";
-
-    if ('f' == mech) {
-    } else { // clean up
-        close_double_vl_as_producer(left_prod_vl);
-        close_double_vl_as_consumer(left_cons_vl);
-        close_double_vl_as_producer(right_prod_vl);
-        close_double_vl_as_consumer(right_cons_vl);
-    }
-
-    return 0;
+    const auto end( std::chrono::high_resolution_clock::now() );
+    const auto time_duration( std::chrono::duration_cast< std::chrono::nanoseconds >( end - start ) ); 
+    
+    std::cout << time_duration.count() << "ns elapsed\n";
+    std::cout << time_duration.count() / round / 2 << "ns average per trip\n";
+        
+#ifdef VL        
+    close_double_vl_as_producer( left_prod_vl   );
+    close_double_vl_as_consumer( left_cons_vl   );
+    close_double_vl_as_producer( right_prod_vl  );
+    close_double_vl_as_consumer( right_cons_vl  );
+#endif
+    return( EXIT_SUCCESS );
 }

7a2ddd86fcb465f7a186648f55892e2578cb2d71
BamboWu <qw2699@utexas.edu>
2019-11-03 20:22:50 -0600

    Improve the script making table

--- scripts/tab_shopping.py
+++ scripts/tab_shopping.py
@@ -11,45 +11,48 @@ def main(argv):
     parser = argparse.ArgumentParser(
             description="Make a table from profiles of shopping benchmark.")
     parser.add_argument("data_dir", metavar="DD", help="Data directory.")
+    parser.add_argument("-n", "--naming", dest="perfout_naming",
+            default="perf{{TEST}}.{{EXE}}",
+            help="How perf output files are named.")
+    parser.add_argument("-t", "--tick", dest="tick_pattern",
+            default="{{TICK}} ticks for MMFan",
+            help="The pattern to catch the value of ticks.")
     arg_list = parser.parse_args(argv[1:])
 
-    naming_pat = "perf{{TEST}}.{{EXE}}"
-    ticks_pat = "{{TICK}} ticks for MMFan"
+    #perfout_naming = "perf{{TEST}}.{{EXE}}" # how perf output files are named
+    #ticks_pat = "{{TICK}} ticks for MMFan"
 
-    file_naming = naming_pat.replace("{{TEST}}", "(?P<test>\d+)")
+    file_naming = arg_list.perfout_naming.replace("{{TEST}}", "(?P<test>\d+)")
     file_naming = file_naming.replace("{{EXE}}", "(?P<exe>\S+)")
     pat_file = re.compile(file_naming)
 
     # list files in directory and determine available test results
-    file_list = os.listdir(arg_list.data_dir)
-    matched_files = list()
-    files_left = list()
-    test_result = list()
-    exe_list = list()
-    for file_name in file_list:
-        resf = pat_file.search(file_name)
+    all_files = os.listdir(arg_list.data_dir)
+    log_files = all_files[:]
+    res_list = list() # results listed per test
+    exe_list = list() # all exe_name caught in perf file naming match
+    for file_name in all_files:
+        resf = pat_file.match(file_name)
         if resf is not None:
             test_idx = int(resf.group("test"))
             exe_name = resf.group("exe")
-            if exe_name not in exe_list:
+            while len(res_list) - 1 < test_idx: # expand the list
+                res_list.append({})
+            res_list[test_idx][exe_name] = { "ticks": 0 }
+            if exe_name not in exe_list: # expand the list
                 exe_list.append(exe_name)
-            while len(test_result) - 1 < test_idx:
-                test_result.append({})
-            test_result[test_idx][exe_name] = { "ticks": 0 }
-            matched_files.append(file_name)
-        else:
-            files_left.append(file_name)
+            log_files.remove(file_name) # match perf output naming won't be log
 
     # recompile pat_file to only match exe_names seen in file naming match
-    exe_str = "(" + ")|(".join(exe_list) + ")"
-    file_naming = naming_pat.replace("{{TEST}}", "(?P<test>\d+)")
-    file_naming = file_naming.replace("{{EXE}}", "(?P<exe>" + exe_str + ")")
+    exe_or_str = "(" + ")|(".join(exe_list) + ")"
+    file_naming = arg_list.perfout_naming.replace("{{TEST}}", "(?P<test>\d+)")
+    file_naming = file_naming.replace("{{EXE}}", "(?P<exe>" + exe_or_str + ")")
     pat_file = re.compile(file_naming)
-    tick_str = ticks_pat.replace("{{TICK}}", "(?P<tick>\d+)")
+    tick_str = arg_list.tick_pattern.replace("{{TICK}}", "(?P<tick>\d+)")
     pat_tick = re.compile(tick_str)
 
     # process the files left from the naming matching for ticks
-    for file_name in files_left:
+    for file_name in log_files:
         fi = open(os.path.join(arg_list.data_dir, file_name))
         for line in fi.readlines():
             resf = pat_file.search(line)
@@ -58,41 +61,42 @@ def main(argv):
                 exe_name = resf.group("exe")
             rest = pat_tick.search(line)
             if rest is not None:
-                ticks = int(rest.group("tick"))
-                test_result[test_idx][exe_name]["ticks"] = ticks
+                res_list[test_idx][exe_name]["ticks"] = int(rest.group("tick"))
         fi.close()
 
     # process the files getting name matched to get perf events statistics
     pat_perf = re.compile("\s+(?P<val>\d{1,3}(?:,\d{3})*)\s+(?P<evt>\S+)\s")
     evt_list = ["ticks"]
-    for test_idx in range(len(test_result)):
-        for exe_name in test_result[test_idx].keys():
-            file_name = naming_pat.replace("{{TEST}}", str(test_idx))
-            file_name = file_name.replace("{{EXE}}", exe_name)
+    for test_idx in range(len(res_list)):
+        test_name = arg_list.perfout_naming.replace("{{TEST}}", str(test_idx))
+        for exe_name in res_list[test_idx].keys():
+            file_name = test_name.replace("{{EXE}}", exe_name)
             fi = open(os.path.join(arg_list.data_dir, file_name))
             for line in fi.readlines():
                 resp = pat_perf.match(line)
                 if resp is not None:
                     perf_val = int(resp.group("val").replace(",", ""))
                     perf_evt = resp.group("evt")
+                    res_list[test_idx][exe_name][perf_evt] = perf_val
                     if perf_evt not in evt_list:
                         evt_list.append(perf_evt)
-                    test_result[test_idx][exe_name][perf_evt] = perf_val
             fi.close()
 
-    # print a table for ticks and all perf events
-    tests_str = ""
-    for i in range(len(test_result)):
-        tests_str = tests_str + "\t1 + " + str(int(2**(i-1)))
-    print("#threads (producer + consumer)\t %s" % tests_str)
+    # print a table for ticks and all perf events, each test per column
+    row_str = "\033[1m#threads (producer + consumer) \t"
+    for i in range(len(res_list)):
+        row_str += "\t1 + " + str(int(2**(i-1)))
+    print(row_str + "\033[0m")
     for evt in evt_list:
+        evt_str = evt
         for exe in exe_list:
-            row_str = evt + "\t" + exe
-            for i in range(len(test_result)):
-                if evt in test_result[i][exe].keys():
-                    row_str = row_str + "\t" + str(test_result[i][exe][evt])
+            row_str = "\033[1m%-23s\t%-15s\033[0m" % (evt_str, exe)
+            evt_str = ""
+            for i in range(len(res_list)):
+                if evt in res_list[i][exe].keys():
+                    row_str += "\t" + str(res_list[i][exe][evt])
                 else:
-                    row_str = row_str + "\t" + "N\A"
+                    row_str += "\t" + "N\A"
             print(row_str)
 
 if "__main__" == __name__:

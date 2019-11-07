bf8ba3a9602bba09af37a230ae7e69a946f9f649
BamboWu <qw2699@utexas.edu>
2019-11-07 12:22:59 -0600

    Port rdtsc() to armv8 architecture

--- src/timing.c
+++ src/timing.c
@@ -5,7 +5,15 @@
  * Read Time Stamp Counter (TSC)
  */
 __inline__ uint64_t rdtsc() {
+#ifdef __x86_64__
   unsigned hi, lo;
-  __asm__ __volatile__ ( "rdtsc" : "=a"(lo), "=d"(hi));
+  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
   return ( (uint64_t)lo) | ( ((uint64_t)hi) << 32);
+#elif __ARM_ARCH == 8
+  uint64_t cntvct;
+  __asm__ __volatile__ ("mrs %0, CNTVCT_EL0" : "=r"(cntvct));
+  return cntvct;
+#else
+  return 0;
+#endif
 }

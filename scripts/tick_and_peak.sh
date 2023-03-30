3551c0623f128131c5dedcc6719b1b6bb6c26556
BamboWu <qw2699@utexas.edu>
2023-03-30 16:19:51 +0000

    Do not monitor memory by default

--- scripts/tick_and_peak.sh
+++ scripts/tick_and_peak.sh
@@ -3,13 +3,25 @@
 proc_name=$(basename $1)
 
 rm -f /tmp/$proc_name.{stdout,stderr,VmPeak}
-$@ > /tmp/$proc_name.stdout 2>/tmp/$proc_name.stderr &
-PID=$!
-while [ -f /proc/$PID/status ]
-do
-    grep ^VmPeak /proc/$PID/status >> /tmp/$proc_name.VmPeak 2>/dev/null
-done
 
-tick=$(awk '/ticks/{ print $1 }' /tmp/$proc_name.stdout)
-vmpeak=$(tail -n 2 /tmp/$proc_name.VmPeak | head -n 1 | awk '{ print $2 }')
-echo "$tick,$vmpeak"
+if [ $MONITOR_MEM_USAGE ]
+then
+
+    $@ > /tmp/$proc_name.stdout 2>/tmp/$proc_name.stderr &
+    PID=$!
+    while [ -f /proc/$PID/status ]
+    do
+        taskset -c 0 grep ^VmPeak /proc/$PID/status >> /tmp/$proc_name.VmPeak 2>/dev/null
+    done
+
+    tick=$(awk '/ticks/{ print $1 }' /tmp/$proc_name.stdout)
+    vmpeak=$(tail -n 2 /tmp/$proc_name.VmPeak | head -n 1 | awk '{ print $2 }')
+    echo "$tick,$vmpeak"
+
+else
+
+    $@ > /tmp/$proc_name.stdout 2>/tmp/$proc_name.stderr
+    tick=$(awk '/ticks/{ print $1 }' /tmp/$proc_name.stdout)
+    echo "$tick"
+
+fi

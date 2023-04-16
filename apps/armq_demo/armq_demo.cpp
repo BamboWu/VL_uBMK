c850a4ef7cd31ad93920e6b620a4d34937f6063a
BamboWu <qw2699@utexas.edu>
2023-04-16 21:44:19 +0000

    Make blocking happen in armq_demo
    The original design of armq_demo turned out to be unlikely blocked (on
    full queues), because of the slow Generator and the fast Filter.
    Therefore, it is difficult to make armq_demo really a good demo for
    ARMQ.
    This commit makes a couple of changes:
     - Let Generator allocate and populate buffers in batch, raising the
     upper bound of message sending rate
     - Replace the sequential buffer access and the useless accumulation in
     Filter with a cacheline-aligned pointer chasing access pattern and
     looping nops to the given number
     - Make incast-like topology

--- apps/armq_demo/armq_demo.cpp
+++ apps/armq_demo/armq_demo.cpp
@@ -22,10 +22,7 @@
 #include <cstdlib>
 #include <cstdint>
 #include <string>
-#include <vector>
-#include <algorithm>
 #include <chrono>
-#include <atomic>
 
 using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
@@ -33,322 +30,31 @@ using std::chrono::nanoseconds;
 
 #include "timing.h"
 
+#include "armq_demo.hpp"
+
+std::size_t *Generator::arrs[ NUM_ARRS ];
+
 #if RAFTLIB_ORIG
 #include "raftlib_orig.hpp"
 #endif
-
-#define NUM_ARRS 1000
-
-struct Chunk
-{
-    Chunk() = default;
-
-    Chunk( const Chunk &other )
-    {
-        (this)->buf = other.buf;
-        (this)->seqnum = other.seqnum;
-    }
-    std::vector< uint8_t > buf;
-    uint64_t seqnum;
-};
-
-class ChunkProcessingKernel : public raft::Kernel
-{
-public:
-    ChunkProcessingKernel( const std::size_t chunk_size ) :
-        raft::Kernel(), size( chunk_size )
-    {
-    }
-
-    virtual ~ChunkProcessingKernel() = default;
-
-#if RAFTLIB_ORIG
-#else
-    virtual bool pop( raft::Task *task, bool dryrun )
-    {
-        return true;
-    }
-
-    virtual bool allocate( raft::Task *task, bool dryrun )
-    {
-        return true;
-    }
-#endif
-
-protected:
-    const std::size_t size;
-};
-
-class Generator : public ChunkProcessingKernel
-{
-public:
-    Generator( const std::size_t chunk_size,
-               const std::size_t delay_ticks,
-               const std::size_t repetitions,
-               int fanout ) :
-        ChunkProcessingKernel( chunk_size ),
-        delay( delay_ticks ),
-        nreps( repetitions )
-    {
-#if RAFTLIB_ORIG
-        for( auto i( 0 ); fanout > i; ++i )
-        {
-            add_output< Chunk >( port_name_of_i( i ) );
-        }
-#else
-        add_output< Chunk >( "0"_port );
-#endif
-        rep = 0;
-        uint8_t *flatted_arr = new uint8_t[ NUM_ARRS * size ];
-        for( auto i( 0 ); NUM_ARRS > i; ++i )
-        {
-            arrs[ i ] = &flatted_arr[ i * size ];
-            for( std::size_t j( 0 ); size > j; ++j )
-            {
-                arrs[ i ][ j ] = ( i + j ) % 52;
-                if( 26 <= arrs[ i ][ j ] )
-                {
-                    arrs[ i ][ j ] += 'G'; /* 'a' - 26 */
-                }
-                else
-                {
-                    arrs[ i ][ j ] += 'A';
-                }
-            }
-        }
-        flatted_arr[ NUM_ARRS + size ] = 0; /* 1 / NUM_ARRS chance to spot 0 */
-    }
-
-    virtual ~Generator()
-    {
-        delete[] arrs[ 0 ];
-    }
-
-#if RAFTLIB_ORIG
-    virtual raft::kstatus run()
-#else
-    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
-                                            raft::StreamingData &bufOut )
-#endif
-    {
-#if RAFTLIB_ORIG
-        for( auto &port : (this)->output )
-        {
-            if( ! port.space_avail() )
-            {
-                continue;
-            }
-            auto beg_tick( rdtsc() );
-            auto &chunk( port.template allocate< Chunk >() );
-#else
-            auto beg_tick( rdtsc() );
-            auto &chunk( bufOut.allocate< Chunk >() );
-#endif
-            chunk.buf.resize( size );
-            for( std::size_t idx( 0 ); size > idx; ++idx )
-            {
-                chunk.buf[ idx ] = arrs[ rep % NUM_ARRS ][ idx ];
-            }
-            chunk.seqnum = rep;
-#if RAFTLIB_ORIG
-            port.send();
-#else
-            bufOut.send();
-#endif
-            while( delay > ( rdtsc() - beg_tick ) )
-            {
-                /* busy doing nothing until delay reached */
-            }
-            if( nreps < ++rep )
-            {
-                return( raft::kstatus::stop );
-            }
-#if RAFTLIB_ORIG
-        }
-#endif
-        return( raft::kstatus::proceed );
-    }
-
-private:
-    const std::size_t delay;
-    const std::size_t nreps;
-    std::size_t rep;
-    uint8_t *arrs[ NUM_ARRS ];
-};
-
-class Filter : public ChunkProcessingKernel
-{
-public:
-    Filter( const std::size_t chunk_size ) :
-        ChunkProcessingKernel( chunk_size )
-    {
-        add_input< Chunk >( "0"_port );
-        add_output< Chunk >( "0"_port );
-        verify_sum =
-            size * size * ( size - 1 ) / 2 -
-            ( size - 1 ) * size * ( 2 * size - 1 ) / 6;
-    }
-
-    Filter( const Filter &other ) :
-        ChunkProcessingKernel( other.size ), verify_sum( other.verify_sum )
-    {
-        add_input< Chunk >( "0"_port );
-        add_output< Chunk >( "0"_port );
-    }
-
-    virtual ~Filter() = default;
-
-#if RAFTLIB_ORIG
-
-    CLONE(); /* enable cloning */
-
-    virtual raft::kstatus run()
-#else
-    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
-                                            raft::StreamingData &bufOut )
-#endif
-    {
-#if RAFTLIB_ORIG
-        auto &chunk( input[ "0"_port ].template peek< Chunk >() );
-#else
-        auto &chunk( dataIn.template peek< Chunk >() );
-#endif
-        std::size_t count_sum = 0;
-        bool seen_zero = false;
-        for( std::size_t idx( 0 ); size > idx; ++idx )
-        {
-            if( 0 == chunk.buf[ idx ] )
-            {
-                seen_zero = true;
-            }
-            for( std::size_t countdown( idx ); 0 < countdown; --countdown )
-            {
-                count_sum += countdown;
-            }
-        }
-        if( count_sum != verify_sum )
-        {
-            std::cout << count_sum << ", " << verify_sum << std::endl;
-        }
-#if RAFTLIB_ORIG
-        if( seen_zero )
-        {
-            output[ "0"_port ].push( chunk );
-        }
-        input[ "0"_port ].recycle();
-#else
-        if( seen_zero )
-        {
-            bufOut.push( chunk );
-        }
-        dataIn.recycle();
-#endif
-        return( raft::kstatus::proceed );
-    }
-
-private:
-
-    std::size_t verify_sum;
-};
-
-class Copy : public ChunkProcessingKernel
-{
-public:
-    Copy( const std::size_t chunk_size, int fanin ) :
-        ChunkProcessingKernel( chunk_size )
-    {
-#if RAFTLIB_ORIG
-        for( auto i( 0 ); fanin > i; ++i )
-        {
-            add_input< Chunk >( port_name_of_i( i ) );
-        }
-#else
-        add_input< Chunk >( "0"_port );
-#endif
-        add_output< Chunk >( "0"_port );
-    }
-
-    virtual ~Copy() = default;
-
-#if RAFTLIB_ORIG
-    virtual raft::kstatus run()
-#else
-    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
-                                            raft::StreamingData &bufOut )
-#endif
-    {
-#if RAFTLIB_ORIG
-        for( auto &port : (this)->input )
-        {
-            if( 0 < port.size() )
-            {
-                auto &chunk( port.template peek< Chunk >() );
-                output[ "0"_port ].push( chunk );
-                port.recycle();
-            }
-        }
-#else
-        auto &chunk( dataIn.template peek< Chunk >() );
-        bufOut.push( chunk );
-        dataIn.recycle();
-#endif
-        return( raft::kstatus::proceed );
-    }
-};
-
-class Count : public ChunkProcessingKernel
-{
-public:
-    Count( const std::size_t chunk_size ) : ChunkProcessingKernel( chunk_size )
-    {
-        add_input< Chunk >( "0"_port );
-    }
-
-    virtual ~Count() = default;
-
-#if RAFTLIB_ORIG
-    virtual raft::kstatus run()
-#else
-    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
-                                            raft::StreamingData &bufOut )
-#endif
-    {
-#if RAFTLIB_ORIG
-        auto &chunk( input[ "0"_port ].template peek< Chunk >() );
-#else
-        auto &chunk( dataIn.template peek< Chunk >() );
-#endif
-        std::size_t cnt = 0;
-        for( auto &val : chunk.buf )
-        {
-            cnt += 'A' == val;
-        }
-        total_cnt.fetch_add( cnt, std::memory_order_relaxed );
-#if RAFTLIB_ORIG
-        input[ "0"_port ].recycle();
-#else
-        dataIn.recycle();
-#endif
-        return( raft::kstatus::proceed );
-    }
-
-    std::atomic< std::size_t > total_cnt = { 0 };
-};
 
 int
 main( int argc, char **argv )
 {
     int repetitions = 1000;
-    int F = 1; /* #filters */
+    int G = 1; /* #generatros/#filters */
     int C = 1; /* #copys */
-    std::size_t D = 10000; /* delay_tick */
-    std::size_t S = 60; /* chunk_size */
+    std::size_t D = 10; /* delay_tick */
+    std::size_t S = 4; /* chunk_size */
+    std::size_t rounds = 2; /* number of rounds touching the buffer */
+    std::size_t nops = 100; /* number of nops */
     if( argc < 2 )
     {
         std::cerr << "Usage: ./" << argv[0] <<
             " <orig|pw|cv|os|mix> [#repetitions=" << repetitions <<
-            "] [F=" << F << "] [C=" << C <<
-            "] [D=" << D << "] [S=" << S << "]\n";
+            "] [G=" << G << "] [C=" << C <<
+            "] [D=" << D << "] [S=" << S <<
+            "] [#rounds=" << rounds << "][#nops=" << nops << "]\n";
         exit( EXIT_FAILURE );
     }
     else if( 2 < argc )
@@ -356,17 +62,25 @@ main( int argc, char **argv )
         repetitions = atoi( argv[ 2 ] );
         if( 3 < argc )
         {
-            F = atoi( argv[ 3 ] );
+            G = atoi( argv[ 3 ] );
             if( 4 < argc )
             {
                 C = atoi( argv[ 4 ] );
                 if( 5 < argc )
                 {
                     D = strtoull( argv[ 5 ], NULL, 0 );
-                    if( 6 < argc )
-                    {
-                        S = strtoull( argv[ 6 ], NULL, 0 );
-                    }
+                }
+                if( 6 < argc )
+                {
+                    S = strtoull( argv[ 6 ], NULL, 0 );
+                }
+                if( 7 < argc )
+                {
+                    rounds = strtoull( argv[ 7 ], NULL, 0 );
+                }
+                if( 8 < argc )
+                {
+                    nops = strtoull( argv[ 8 ], NULL, 0 );
                 }
             }
         }
@@ -381,19 +95,35 @@ main( int argc, char **argv )
     {
         exit( EXIT_FAILURE );
     }
-    if( 1 > F || 1 > C )
+    if( 1 > G || 1 > C )
     {
         exit( EXIT_FAILURE );
     }
 
-    Generator gen( S, D, repetitions, F );
-    Filter filter( S );
+#if DUPLICATE_FILTERS
+    int F = G;
+#else
+    int F = 1;
+#endif
+
+    std::cout << runtime_choice << " #repetitions=" << repetitions <<
+        " G=" << G << " F=" << F << " C=" << C << " D=" << D << " S=" << S <<
+        " #rounds=" << rounds << " #nops=" << nops << std::endl;
+
+    Sequencer seq( repetitions, G );
+    Generator::populate( S );
+    Generator gen( S, D );
+    Filter filter( S, rounds, nops, G / F );
     Copy copy( S, F );
     Count count( S );
     raft::DAG dag;
 
 #if RAFTLIB_ORIG
-    auto kernels( dag += gen <= filter >= copy );
+#if DUPLICATE_FILTERS
+    auto kernels( dag += seq <= gen >> filter >= copy );
+#else
+    auto kernels( dag += seq <= gen >= filter >> copy );
+#endif
     for( auto i( 1 ); C > i; ++i )
     {
         kernels = ( dag += kernels.getDst().first->get() >>
@@ -401,10 +131,11 @@ main( int argc, char **argv )
     }
     dag += kernels.getDst().first->get() >> count;
 #else
-    raft::Kpair *kpair( &( gen >> filter * F >> copy * 0 ) );
+    raft::Kpair *kpair( &( seq >> ( gen * G >> filter * F ) >> copy * 0 ) );
     for( auto i( 1 ); C > i; ++i )
     {
-        kpair = &( *kpair >> *raft::kernel_maker< Copy >( S, 1 ) * 0 );
+        kpair = &( *kpair >>
+                   *raft::kernel_maker< Copy >( S, 1 ) * 0 );
     }
     dag += *kpair >> count * 0;
 #endif
@@ -429,25 +160,25 @@ main( int argc, char **argv )
 #endif
              no_parallel >();
 #else // not( RAFTLIB_ORIG )
-    if( "mix" == runtime_choice )
+    if( "mixcv" == runtime_choice )
     {
-        std::cout << "Mix\n";
-        dag.exe< raft::RuntimeMix >();
+        dag.exe< raft::RuntimeGroupMixCV >();
+    }
+    else if( "cv" == runtime_choice )
+    {
+        dag.exe< raft::RuntimeGroupFIFOCV >();
     }
     else if ( "pw" == runtime_choice )
     {
-        std::cout << "PW\n";
-        dag.exe< raft::RuntimeFIFO >();
+        dag.exe< raft::RuntimeGroupFIFO >();
     }
     else if ( "os" == runtime_choice )
     {
-        std::cout << "OneShot\n";
-        dag.exe< raft::RuntimeNewBurst >();
+        dag.exe< raft::RuntimeGroupNewBurst >();
     }
     else
     {
-        std::cout << "CV\n";
-        dag.exe< raft::RuntimeFIFOCV >();
+        dag.exe< raft::RuntimeGroupMix >();
     }
 #endif
 
---
+++ apps/armq_demo/armq_demo.hpp
@@ -0,0 +1,447 @@
+/**
+ * armq_demo.hpp -
+ * @author: Qinzhe Wu
+ * @version: Mon Apr 24 17:40:03 2023
+ *
+ * Copyright 2023 The Regents of the University of Texas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARMQ_DEMO_HPP__
+#define ARMQ_DEMO_HPP__
+#include <raft>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <atomic>
+
+#if RAFTLIB_ORIG
+#include "raftlib_orig.hpp"
+#endif
+
+#define NUM_ARRS 10000
+#ifndef L1D_CACHE_LINE_SIZE
+#define L1D_CACHE_LINE_SIZE 64
+#endif
+
+#define NVALS_PER_LINE ( L1D_CACHE_LINE_SIZE / sizeof( std::size_t ) )
+
+struct Chunk
+{
+    Chunk() = default;
+
+    Chunk( const Chunk &other )
+    {
+        (this)->buf = other.buf;
+        (this)->seqnum = other.seqnum;
+    }
+    std::size_t *buf;
+    std::size_t seqnum;
+};
+
+class ChunkProcessingKernel : public raft::Kernel
+{
+public:
+    ChunkProcessingKernel( const std::size_t chunk_size ) :
+        raft::Kernel(), size( chunk_size )
+    {
+    }
+
+    virtual ~ChunkProcessingKernel() = default;
+
+#if RAFTLIB_ORIG
+#else
+    virtual bool pop( raft::Task *task, bool dryrun )
+    {
+        return true;
+    }
+
+    virtual bool allocate( raft::Task *task, bool dryrun )
+    {
+        return true;
+    }
+#endif
+
+protected:
+
+    void touch( Chunk &chunk, std::size_t nrounds, std::size_t nnops )
+    {
+        std::size_t nop_rounds( nnops / nrounds / 10 );
+        for( std::size_t i( 0 ); nrounds > i; ++i )
+        {
+            std::size_t pos = 0;
+            // a chasing pointer pattern to create some cache pressure
+            do
+            {
+                pos = chunk.buf[ pos ];
+            } while( 0 != pos );
+            // add a bit more work by nops
+            for( std::size_t j( 0 ); nop_rounds > j; ++j )
+            {
+                __asm__ volatile( "\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        nop \n\
+                        "
+                        :
+                        :
+                        : );
+            }
+        }
+    }
+
+    const std::size_t size;
+};
+
+class Sequencer : public raft::Kernel
+{
+public:
+    Sequencer( const std::size_t repetitions, int fanout ) :
+        raft::Kernel(), nreps( repetitions )
+    {
+#if RAFTLIB_ORIG
+        for( auto i( 0 ); fanout > i; ++i )
+        {
+            add_output< std::size_t >( port_name_of_i( i ) );
+        }
+#else
+        add_output< std::size_t >( "0"_port );
+#endif
+        rep = 0;
+    }
+
+    virtual ~Sequencer() = default;
+
+#if RAFTLIB_ORIG
+    virtual raft::kstatus run()
+#else
+    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
+                                            raft::StreamingData &bufOut )
+#endif
+    {
+#if RAFTLIB_ORIG
+        for( auto &port : (this)->output )
+        {
+            if( ! port.space_avail() )
+            {
+                continue;
+            }
+            port.push( rep );
+#else
+            bufOut.push( rep );
+#endif
+            if( nreps < ++rep )
+            {
+                return( raft::kstatus::stop );
+            }
+#if RAFTLIB_ORIG
+        }
+#endif
+        return( raft::kstatus::proceed );
+    }
+
+#if RAFTLIB_ORIG
+#else
+    virtual bool pop( raft::Task *task, bool dryrun )
+    {
+        return true;
+    }
+
+    virtual bool allocate( raft::Task *task, bool dryrun )
+    {
+        return true;
+    }
+#endif
+
+private:
+    const std::size_t nreps;
+    std::size_t rep;
+};
+
+class Generator : public ChunkProcessingKernel
+{
+public:
+    Generator( const std::size_t chunk_size,
+               const std::size_t delay_ticks ) :
+        ChunkProcessingKernel( chunk_size ),
+        delay( delay_ticks )
+    {
+        add_input< std::size_t >( "0"_port );
+        add_output< Chunk >( "0"_port );
+    }
+
+    Generator( const Generator &other ) :
+        ChunkProcessingKernel( other.size ),
+        delay( other.delay )
+    {
+        add_input< std::size_t >( "0"_port );
+        add_output< Chunk >( "0"_port );
+    }
+
+    virtual ~Generator() = default;
+
+#if RAFTLIB_ORIG
+
+    CLONE(); /* enable cloning */
+
+    virtual raft::kstatus run()
+#else
+    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
+                                            raft::StreamingData &bufOut )
+#endif
+    {
+        std::size_t seqnum;
+#if RAFTLIB_ORIG
+        input[ "0"_port ].pop( seqnum );
+#else
+        dataIn.pop( seqnum );
+#endif
+        auto beg_tick( rdtsc() );
+#if RAFTLIB_ORIG
+        auto &chunk( output[ "0"_port ].template allocate< Chunk >() );
+#else
+        auto &chunk( bufOut.allocate< Chunk >() );
+#endif
+        chunk.buf = arrs[ seqnum % NUM_ARRS ];
+        chunk.seqnum = seqnum;
+        while( delay > ( rdtsc() - beg_tick ) )
+        {
+            /* busy doing nothing until delay reached */
+        }
+#if RAFTLIB_ORIG
+        output[ "0"_port ].send();
+#else
+        bufOut.send();
+#endif
+        return( raft::kstatus::proceed );
+    }
+
+    static void populate( const std::size_t size )
+    {
+        srand( size );
+        std::size_t *flatted_arr =
+            new std::size_t[ NUM_ARRS * size * NVALS_PER_LINE ];
+        std::size_t pos;
+        // create a pool of offsets
+        std::vector< std::size_t > offsets( size );
+        for( std::size_t i( 0 ); size > i; ++i )
+        {
+            offsets[ i ] = i;
+        }
+        for( std::size_t i( 0 ); NUM_ARRS > i; ++i )
+        {
+            // make arrs[i] the shortcuts to the i-th segement
+            arrs[ i ] = &flatted_arr[ i * size * NVALS_PER_LINE ];
+
+            // populate the arrs[ i ] to form a chain
+            pos = 0; // begin with position 0
+            for( std::size_t j( 0 ); ( size - 1 ) > j; ++j )
+            {
+                std::size_t rand_idx;
+                do
+                {
+                    rand_idx = rand() % ( size - j );
+                } while( 0 == rand_idx || // do not point to 0
+                         offsets[ rand_idx ] == pos ); // do not point to self
+                arrs[ i ][ pos * NVALS_PER_LINE ] =
+                    offsets[ rand_idx ] * NVALS_PER_LINE;
+                pos = offsets[ rand_idx ];
+                // swap the used offset to tail, so it would not be used again
+                std::swap( offsets[ rand_idx ], offsets[ size - j - 1 ] );
+            }
+            arrs[ i ][ pos * NVALS_PER_LINE ] = 0;
+            // last slot unset loops back to position 0
+        }
+    }
+
+private:
+
+    const std::size_t delay;
+    static std::size_t *arrs[ NUM_ARRS ];
+};
+
+class Filter : public ChunkProcessingKernel
+{
+public:
+    Filter( const std::size_t chunk_size,
+            const std::size_t rounds,
+            const std::size_t nops,
+            int fanin ) :
+        ChunkProcessingKernel( chunk_size ),
+        nrounds( rounds ),
+        nnops( nops )
+    {
+#if RAFTLIB_ORIG
+        for( auto i( 0 ); fanin > i; ++i )
+        {
+            add_input< Chunk >( port_name_of_i( i ) );
+        }
+#else
+        add_input< Chunk >( "0"_port );
+#endif
+        add_output< Chunk >( "0"_port );
+    }
+
+    Filter( const Filter &other ) :
+        ChunkProcessingKernel( other.size ),
+        nrounds( other.nrounds ),
+        nnops( other.nnops )
+    {
+        add_input< Chunk >( "0"_port );
+        add_output< Chunk >( "0"_port );
+    }
+
+    virtual ~Filter() = default;
+
+#if RAFTLIB_ORIG
+
+    CLONE(); /* enable cloning */
+
+    virtual raft::kstatus run()
+#else
+    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
+                                            raft::StreamingData &bufOut )
+#endif
+    {
+#if RAFTLIB_ORIG
+        for( auto &port : (this)->input )
+        {
+            if( 0 < port.size() )
+            {
+                auto &chunk( port.template peek< Chunk >() );
+                touch( chunk, nrounds, nnops );
+                if( ( ( chunk.seqnum / 1000 ) % 1000 ) ==
+                    ( chunk.seqnum % 1000 ) )
+                {
+                    output[ "0"_port ].push( chunk );
+                }
+                port.recycle();
+            }
+        }
+#else
+        auto &chunk( dataIn.template peek< Chunk >() );
+        touch( chunk, nrounds, nnops );
+        if( ( ( chunk.seqnum / 1000 ) % 1000 ) == ( chunk.seqnum % 1000 ) )
+        {
+            bufOut.push( chunk );
+        }
+        dataIn.recycle();
+#endif
+        return( raft::kstatus::proceed );
+    }
+private:
+    const std::size_t nrounds;
+    const std::size_t nnops;
+};
+
+class Copy : public ChunkProcessingKernel
+{
+public:
+    Copy( const std::size_t chunk_size,
+          int fanin ) :
+        ChunkProcessingKernel( chunk_size )
+    {
+#if RAFTLIB_ORIG
+        for( auto i( 0 ); fanin > i; ++i )
+        {
+            add_input< Chunk >( port_name_of_i( i ) );
+        }
+#else
+        add_input< Chunk >( "0"_port );
+#endif
+        add_output< Chunk >( "0"_port );
+    }
+
+    virtual ~Copy() = default;
+
+#if RAFTLIB_ORIG
+    virtual raft::kstatus run()
+#else
+    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
+                                            raft::StreamingData &bufOut )
+#endif
+    {
+#if RAFTLIB_ORIG
+        for( auto &port : (this)->input )
+        {
+            if( 0 < port.size() )
+            {
+                auto &chunk( port.template peek< Chunk >() );
+                touch( chunk, 1, 0 );
+                output[ "0"_port ].push( chunk );
+                port.recycle();
+            }
+        }
+#else
+        auto &chunk( dataIn.template peek< Chunk >() );
+        touch( chunk, 1, 0 );
+        bufOut.push( chunk );
+        dataIn.recycle();
+#endif
+        return( raft::kstatus::proceed );
+    }
+};
+
+class Count : public ChunkProcessingKernel
+{
+public:
+    Count( const std::size_t chunk_size ) : ChunkProcessingKernel( chunk_size )
+    {
+        add_input< Chunk >( "0"_port );
+    }
+
+    virtual ~Count() = default;
+
+#if RAFTLIB_ORIG
+    virtual raft::kstatus run()
+#else
+    virtual raft::kstatus::value_t compute( raft::StreamingData &dataIn,
+                                            raft::StreamingData &bufOut )
+#endif
+    {
+#if RAFTLIB_ORIG
+        auto &chunk( input[ "0"_port ].template peek< Chunk >() );
+#else
+        auto &chunk( dataIn.template peek< Chunk >() );
+#endif
+        std::size_t cnt = 0;
+        std::size_t pos = 0;
+        do
+        {
+            cnt += 0 == pos;
+            pos = chunk.buf[ pos ];
+        } while( 0 != pos );
+        total_cnt.fetch_add( cnt, std::memory_order_relaxed );
+        //delete[] chunk.buf;
+#if RAFTLIB_ORIG
+        input[ "0"_port ].recycle();
+#else
+        dataIn.recycle();
+#endif
+        return( raft::kstatus::proceed );
+    }
+
+    std::atomic< std::size_t > total_cnt = { 0 };
+};
+
+#endif /* end of ARMQ_DEMO_HPP__ */
